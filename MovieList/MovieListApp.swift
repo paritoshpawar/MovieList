c20face68bce276ce7011539a81b4b1f5420178a
pawarparitosh <Paritosh.Pawar@Amway.com>
2023-11-25 12:44:33 +0530

    Initial commit after project creation

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Paritosh on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
